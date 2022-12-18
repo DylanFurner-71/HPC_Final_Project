ac97479434ca90c976ad39f4c1723baf28b1afb7
Paul Rizzolo <111449711+prizz111@users.noreply.github.com>
2022-12-18 22:40:11 +0000

    Added split_results

---
+++ split_results.py
@@ -0,0 +1,12 @@
+lines_per_file = 1000000
+smallfile = None
+with open('tfidfall.txt') as bigfile:
+    for lineno, line in enumerate(bigfile):
+        if lineno % lines_per_file == 0:
+            if smallfile:
+                smallfile.close()
+            small_filename = 'small_file_{}.txt'.format(lineno + lines_per_file)
+            smallfile = open(small_filename, "w")
+        smallfile.write(line)
+    if smallfile:
+        smallfile.close()
